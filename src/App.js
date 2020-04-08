402bf7b5b6776759e988bd442b7aa7773118bee2
Raykard Aguiar <raykjdev@gmail.com>
2020-04-07 21:58:36 -0300

    feature: add routes get, add and remove repos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api"
 
 import "./styles.css";
 
 function App() {
+
+  const [projects, setProject] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await api.get(`repositories`)
+      setProject([...data])
+    }
+    fetchData()
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const prepareBody = {
+      title: `Novo item ${Date.now()}`,
+      techs: ['NodeBolado', 'ReactAvassalador'],
+      url: "https://github.com/Rocketseat/umbriel",
+    }
+    try {
+      const { data } = await api.post(`repositories`, prepareBody)
+      setProject([...projects, data])
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`)
+      const removeItem = projects.filter(project => project.id !== id)
+      setProject([...removeItem])
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
-    <div>
+    <div className="main-repository">
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {projects.map(project => <li key={project.id}>
+          <span>{project.title}</span>
+          <button onClick={() => handleRemoveRepository(project.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
